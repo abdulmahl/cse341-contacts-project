8355e9ab9f360204559faa7e984c8e45e80325f4
Abdul Mahl <mdu_abdul@icloud.com>
2024-01-19 10:33:14 +0200

    Enlarged the home page msg.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ router.use("/", require("./swagger"));
 
 router.get("/", (req, res) => {
   //#swagger.tags=["I Got Swagger"]
-  res.send("Hello Swagger Contacts");
+  res.send(
+    "<center><h1 style='color:crimson; padding-top: 15rem; font-size:6rem'>This is the CSE 341 Contacts Project Home Page: Part-2</h1></center>"
+  );
 });
 
 router.use("/contacts", require("./router"));
