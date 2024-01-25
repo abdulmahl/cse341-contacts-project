4949d6cf4b0518755bf17c9473929ea6c5112b0f
Abdul Mahl <mdu_abdul@icloud.com>
2024-01-25 10:48:18 +0200

    .

--- server.js
+++ server.js
@@ -8,16 +8,14 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
-  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
   );
-  res.status(200);
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+  );
   next();
 });
 
