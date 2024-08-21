cc545bb21d87282abf49eeec2606340bcd82af1a
Abdul Mahl <mdu_abdul@icloud.com>
2024-08-21 22:04:36 +0200

    Removed redundant middleware.

--- server.js
+++ server.js
@@ -7,18 +7,6 @@ require("./database/connect");
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
-  );
-  next();
-});
-
 app.use("/", require("./routes"));
 
 app.listen(port, () => {
